207fca8c1279d0d4dd25551e12328ce9a75618b5
Russell Strauss <russellstrauss@gmail.com>
2019-03-27 21:18:05 -0400

    Adding UX category

--- wp-content/themes/minimal/work.php
+++ wp-content/themes/minimal/work.php
@@ -8,38 +8,21 @@
  */
 
 get_header(); ?>
-
-	<div class="work">
-		<div class="coming-soon">Coming soon!</div>
-	</div>
 	
-	<script type="text/javascript">
-		jQuery(document).ready(function() {
-			jQuery('#category-container').slideDown(1500, 'easeInOutExpo');
-		});
-		
-	</script>
 	<div id="category-container">
 		
 		<?php query_posts( 'post_type=portfolio-category&order=ASC&posts_per_page=100' ); ?>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<div class="portfolio-category">
-											
-				<?php $link = get_bloginfo('url') . '/work/' . str_replace(' ', '-', strtolower(get_the_title()) );?>
-				<?php if (get_the_title() == "Other") {?>
-					
-					<a class="unfinished">
-						<?php the_title(); ?>								
-					</a>
+				
 				<?php
-				}
-				else { ?>
-					
-					<a href="<?php echo $link; ?>">
-						<?php the_title(); ?>								
-					</a>
-				<?php
-				} ?>
+					$post_slug = $post->post_name;
+					$link = get_bloginfo('url') . '/work/' . substr($post_slug, strrpos($post_slug, '/'));
+				?>
+	
+				<a href="<?php echo $link; ?>">
+					<?php the_title(); ?>								
+				</a>
 				
 			 </div>
 		<?php endwhile; 
@@ -48,6 +31,5 @@ get_header(); ?>
 		<div style="clear: both;"></div>
 							
 	</div>
-
 
 <?php get_footer(); ?>
