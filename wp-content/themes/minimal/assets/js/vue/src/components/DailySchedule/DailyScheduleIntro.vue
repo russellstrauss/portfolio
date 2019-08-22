bc9e336ec41e4dfb32e81a4d882543595109ef78
Russell Strauss <russellstrauss@gmail.com>
2019-08-21 23:32:21 -0400

    tweaking nav and adding daily schedule content

--- wp-content/themes/minimal/assets/js/vue/src/components/DailySchedule/DailyScheduleIntro.vue
+++ wp-content/themes/minimal/assets/js/vue/src/components/DailySchedule/DailyScheduleIntro.vue
@@ -5,6 +5,8 @@
 		<h5 v-if="subtitle" class="subtitle">{{ subtitle }}</h5>
 		
 		<h2>{{ title }}</h2>
+
+		<div class="body" v-if="introText" v-html="introText"></div>
 		
 		<div class="date-selector-and-park-hours">
 			<DailyScheduleDateSelector :layout="layout" v-on:date-prev="$emit('date-prev');" v-on:date-next="$emit('date-next');"></DailyScheduleDateSelector>
@@ -24,7 +26,7 @@
 			DailyScheduleDateSelector
 		},
 		
-		props: ['layout', 'subtitle', 'title', 'scheduleDisclaimer', 'dates'],
+		props: ['layout', 'subtitle', 'introText', 'title', 'scheduleDisclaimer', 'dates'],
 		
 		computed: {
 			
