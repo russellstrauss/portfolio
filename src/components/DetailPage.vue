4f97086146dc65751ff658b052fd82454b7754b4
Russell Strauss <russellstrauss@gmail.com>
2020-09-08 01:03:11 -0400

    fix small bug on front page anim

---
+++ src/components/DetailPage.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="detail-page">
+		
+		<div class="layout-wrapper">
+			<div>
+				<h1>{{ category.title }}</h1>
+				<p>{{ category.description }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	
+	import axios from 'axios';
+	
+	export default {
+		
+		name: 'DetailPage',
+
+		components: {},
+
+		data() {
+			return {
+				details: {
+					title: "",
+					path: "",
+					description: "",
+					markup: ""
+				}
+			};
+		},
+
+		methods: {},
+
+		mounted: function () {
+			
+			let self = this;
+			let pieces = '/data/pieces.json';
+			axios.get(pieces).then(function(response) {
+				
+				// let pieces = response.data.pieces;
+				// self.category = pieces.filter(category => category.path === self.$route.params.path)[0];
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		}
+	};
+</script>
+
+<style lang="scss">
+
+</style>
