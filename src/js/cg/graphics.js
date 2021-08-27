fd29f15f3fb3a80fd086fc6f55de8ef69b79d819
Russell Strauss <russellstrauss@gmail.com>
2021-08-26 18:44:57 -0600

    Update to Buffer Geometry and fix colors

--- src/js/cg/graphics.js
+++ src/js/cg/graphics.js
@@ -478,6 +478,33 @@ var gfx = (function() {
 			else {
 				return null;
 			}
+		},
+		
+		consoleLogColor: function(color) { // three.js Color
+			var argArray = [];
+			
+			console.log('#' + color.getHexString())
+			
+			let markup = '<span style="background-color:#' + color.getHexString() + ';"> Hex #' + color.getHexString() + ';' + '' + ' RGB(' + color.r +', ' + color.g + ', ' + color.b + ') </span>';
+		
+			if (arguments.length) {
+				var startTagRe = /<span\s+style=(['"])([^'"]*)\1\s*>/gi;
+				var endTagRe = /<\/span>/gi;
+		
+				var reResultArray;
+				argArray.push(markup.replace(startTagRe, '%c').replace(endTagRe, '%c'));
+				while (reResultArray = startTagRe.exec(markup)) {
+					argArray.push(reResultArray[2]);
+					argArray.push('');
+				}
+		
+				// pass through subsequent args since chrome dev tools does not (yet) support console.log styling of the following form: console.log('%cBlue!', 'color: blue;', '%cRed!', 'color: red;');
+				for (var j = 1; j < arguments.length; j++) {
+					argArray.push(arguments[j]);
+				}
+			}
+		
+			console.log.apply(console, argArray);
 		}
 	}
 })();
