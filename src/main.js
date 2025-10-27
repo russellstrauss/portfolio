769e0cd1e1ca3103e1f059f3ec2eb40928fd02b5
Russell Strauss <russellstrauss@gmail.com>
2025-10-26 21:49:18 -0400

    Upgrade to Node v20 compatibility
    Upgrade to Node v20 compatibility

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import './sass/main.scss'; // remove?
+//import './sass/main.scss'; // remove?
 import VueAwesomeSwiper from 'vue-awesome-swiper';
 
 import App from './App.vue';
