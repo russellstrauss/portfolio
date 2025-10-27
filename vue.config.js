39e3df27fb545adebdb3bf2d23e3fd921b6b6bc9
Russell Strauss <russellstrauss@gmail.com>
2025-10-27 00:49:53 -0400

    Vue 3 upgrade

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,15 @@
+const webpack = require('webpack');
+
 module.exports = {
 	// You also need to upload /.htaccess to the server root directory for routing to function correctly.
 	publicPath: process.env.NODE_ENV === 'production' ? 'https://portfolio.jrstrauss.net/' : '/', // if uploading to subfolder in domain, add it here, otherwise just use '' or '/'. Also update main.js to match
 	configureWebpack: {
 		output: {
-			filename: 'app.js'
-		}
+			filename: '[name].js' // ✅ resolves chunk conflict
+		},
+		plugins: [
+			new webpack.ProgressPlugin() // ✅ override legacy plugin
+		]
 	},
 	css: {
 		loaderOptions: {
