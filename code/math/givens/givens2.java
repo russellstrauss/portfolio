8f329059797d5308860622d6c92b360cd9f3623f
Russell Strauss <russellstrauss@gmail.com>
2025-12-01 01:23:33 -0500

    Deploy to GitHub Pages [skip ci]

--- code/math/givens/givens2.java
+++
@@ -1,9 +0,0 @@
-// The for loops that begin the Givens rotation matrices.
- 
-for (int i=0; i<n; i++) {
-        for (int j=(n-1); j>i; j--) {                                       
-             
-            a = An[j-1][i];
-            b = An[j][i];   
-            cosX = a/(Math.sqrt(a*a+b*b));
-            sinX = -b/(Math.sqrt(a*a+b*b));
