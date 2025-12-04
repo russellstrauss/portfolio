2759b8fcff2ad5f33990c71f02cde9619b4bfb19
Russell Strauss <russellstrauss@gmail.com>
2025-12-03 23:09:28 -0500

    Deploy to GitHub Pages [skip ci]

--- dist/code/math/matrix-operations/norm-vector.java
+++
@@ -1,9 +0,0 @@
-// Calculates the norm of a passed in vector, as a normal array.
-public static double norm(double[] Vector) {
-    double result = 0;
-    for (int i=0; i<Vector.length; i++) {
-        result = result + (Vector[i] * Vector[i]);      
-    }
-    result = Math.sqrt(result);
-    return result;
-}
