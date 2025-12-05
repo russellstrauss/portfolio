bed6d73dfd8ad34daabc4d419e964f62d1601f6d
Russell Strauss <russellstrauss@gmail.com>
2025-12-05 12:38:59 -0500

    Deploy to GitHub Pages [skip ci]

--- dist/code/math/matrix-operations/subtract-vectors.java
+++
@@ -1,9 +0,0 @@
-// Will subtract Vector2 from Vector1.
-public static double[] subtractVector(double[] Vector1, double[] Vector2) {
-    double n = Vector1.length;
-    double[] result = new double[(int)n];
-    for (int i = 0; i<n; i++) {
-        result[i] = Vector1[i] - Vector2[i];
-    }
-    return result;  
-}
