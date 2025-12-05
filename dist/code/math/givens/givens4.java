d90c7dc89fbe47779f5776e47477ac14d0a8de68
Russell Strauss <russellstrauss@gmail.com>
2025-12-04 23:40:41 -0500

    Deploy to GitHub Pages [skip ci]

--- dist/code/math/givens/givens4.java
+++
@@ -1,9 +0,0 @@
-			System.out.println("G" + iteration + ":");
-			printMatrix(Gn);            
-			
-			An = matrixMultiplication(Gn, An);
-			
-			System.out.println("A" + iteration + ":");
-			printMatrix(An);                
-			 
-			Q = matrixMultiplication(Gn, Q);
