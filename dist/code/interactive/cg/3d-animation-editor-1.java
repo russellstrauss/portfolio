a9208c1e4246ac112cb1724dcb2b7122d4245f40
Russell Strauss <russellstrauss@gmail.com>
2025-12-05 19:00:02 -0500

    Deploy to GitHub Pages [skip ci]

---
+++ dist/code/interactive/cg/3d-animation-editor-1.java
@@ -0,0 +1,3 @@
+for (int i=0; i<_B.getNumberOfBalls; i++) {
+	arrayOfCurves[0] = { keyframe1[i], keyframe2[i], keyframe3[i], keyframe4[i], keyframe5[i], keyframe6[i], keyframe7[i], keyframe8[i] };
+}
