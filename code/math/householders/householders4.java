c244e82329320f352a068df5588a5d8205e35e36
Russell Strauss <russellstrauss@gmail.com>
2025-12-05 19:27:16 -0500

    Deploy to GitHub Pages [skip ci]

--- code/math/householders/householders4.java
+++
@@ -1,4 +0,0 @@
-double[][] QRMinusH = subtractMatrix(Z, Hilbert2);
-double InfNormofQRMinusH = normOfInfinity(QRMinusH);
-System.out.println("The infinite norm of [QR - H]:");
-System.out.println(InfNormofQRMinusH);
