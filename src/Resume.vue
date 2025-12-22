d9b150d45bb4466c437938abaf6634b5aa044388
Russell Strauss <russellstrauss@gmail.com>
2025-12-22 00:45:29 -0500

    adjust resume

--- src/Resume.vue
+++ src/Resume.vue
@@ -92,7 +92,7 @@
 				<h2>Skills</h2>
 				<ul class="plus">
 					<li>Front end: JavaScript, React, Vue, Angular, UI/UX, NPM, SASS, Three.js, D3.js, responsive, accessibility (WCAG)</li>
-					<li>Interaction & Visualization: VR, 3D experiences, data visualization, animation</li>
+					<li>Interactive & Visualization: VR, 3D experiences, data visualization, animation</li>
 					<li>Back end: C#, Node, .Net, Python</li>
 					<li>DevOps: CI/CD, Azure, Google Cloud, AWS, CloudFlare</li>
 					<li>CMS: SiteCore, WordPress, Drupal</li>
